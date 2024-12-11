13c77023f156a13144a1e49968d9436e6d760df6
rav000 <97004117+rav000@users.noreply.github.com>
2024-12-11 19:55:21 +0700

    boot patches

--- boot.sh
+++ boot.sh
@@ -1,18 +1,18 @@
-./boot/gaster pwn 
-sleep 2
 ./boot/ipwnder32 -p
 sleep 2
+./boot/gaster pwn 
+sleep 2
 ./boot/gaster reset
-irecovery -f iBSS.img4
+irecovery -f ./boot/iBSS.img4
 if [ -f "./boot/ipwnder32" ]; then
-    irecovery -f iBSS.img4
+    irecovery -f ./boot/iBSS.img4
 fi
-irecovery -f iBEC.img4
-irecovery -f devicetree.img4
+irecovery -f ./boot/iBEC.img4
+irecovery -f ./boot/devicetree.img4
 irecovery -c devicetree
 if [ -f "trustcache.img4" ]; then
     irecovery -f trustcache.img4
     irecovery -c firmware
 fi
-irecovery -f kernelcache.img4
+irecovery -f ./boot/kernelcache.img4
 irecovery -c bootx
