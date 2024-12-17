394f74adcd67f95b80d2aab25a8d049674e627c0
rav000 <97004117+rav000@users.noreply.github.com>
2024-12-17 14:43:37 +0700

    meow

--- make_bootchain.sh
+++ make_bootchain.sh
@@ -2,11 +2,12 @@ echo "Patching iBSS"
 ./bin/iBoot64Patcher ./boot/iBSS.dec ./boot/iBSS.patched
 ./bin/img4 -i ./boot/iBSS.patched -o ./boot/iBSS.img4 -M IM4M -A -T ibss
 echo "Patching iBEC"
-./bin/iBoot64Patcher /boot/iBEC.dec ./boot/iBEC.patched -b "rd=disk0s1s4 wdt=-1 debug=0x2014e -v"
-./bin/img4 -i ./boot/iBSS.patched -o ./boot/iBSS.img4 -M IM4M -A -T ibss
+./bin/iBoot64Patcher ./boot/iBEC.dec ./boot/iBEC.patched -b "rd=disk0s1s4 wdt=-1 debug=0x2014e -v"
+./bin/img4 -i ./boot/iBEC.patched -o ./boot/iBEC.img4 -M IM4M -A -T ibss
 
 echo "Patching kernelcache"
 ./bin/img4 -i kernelcache.release -o kcache.raw
+cp kcache.raw ./boot/kcache.raw
 ./bin/seprmvr64 ./boot/kcache.raw ./boot/kcache.patched
 ./bin/sepless ./boot/kcache.patched ./boot/kcache.patched2
 ./bin/kerneldiff ./boot/kcache.raw ./boot/kcache.patched2 kc.bpatch
